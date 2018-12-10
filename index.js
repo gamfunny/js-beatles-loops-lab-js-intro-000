7c62b100ca67d4db6038b2d00cd7e8f5e7be3e91
gam funnie <gamfunny@gmail.com>
2018-12-10 03:59:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// add solution here
+const singers = ["John Lennon", "Ringo Star", "Paul McCartney", "Dizzy"];
+const instruments = ["guitar", "drums", "piano", "trumpet"];
+ var theBeatlesPlay = function (musiciansArray, instrumentsArray) {
+	var collect = [];
+	for (var i = 0; i<musiciansArray.length; i++) {
+	collect.push(musiciansArray[i] + " plays " + instrumentsArray[i]);
+	}
+	return(collect);
+};
+ theBeatlesPlay(singers, instruments);
